19217dceac10b9ed0518fdbb788a126f941eb8f2
bubu <bubu@bubu-MacBook-Pro.local>
2020-06-29 18:36:02 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect, url_for, render_template, request
+import os
+from flask import Flask, redirect, flash, url_for, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail, Message
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__ , template_folder = './templates')
 
 app.secret_key = 'YourSuperSecreteKey'
+
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'doc', 'docx'}
 
 # add mail server config
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -13,6 +18,7 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'prekladyproskoly@gmail.com' 
 app.config['MAIL_PASSWORD'] = 'dftifkrliunfzxqt'
 app.config['MAIL_ASCII_ATTTACHMENTS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mail = Mail(app)
 
@@ -24,20 +30,67 @@ def home():
 def onas():
 	return render_template('o-nas.html')
 
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/contactform', methods = ['GET', 'POST'])
 def contactform():
+	
 
 	if request.method == 'POST':
+
+
 		user_name = str(request.form)
 		
+		sender_email = request.form['email']
 
 		msg = Message("Message from your visitor" + user_name,
-	                    sender='YourUser@NameHere',
+	                    sender=sender_email,
 	                    recipients=['prekladyproskoly@gmail.com'])
 		msg.body = (user_name)
+
+		if "attachment" not in request.files:
+			flash('No file part')
+			return "where my file"
+		file = request.files['attachment']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+		with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'rb') as fp:
+			msg.attach("image.png", "image/png", fp.read())
+
+
+
+
+			# file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			# return redirect(url_for('uploaded_file',
+			# 						filename=filename))
 		
-		with open(request.form['attachment'], 'rb') as f:
-			msg.attach("somefile", 'image/jpg', f.read())
+	
+   
+
+
+		# <sends messages>
+
+		# msg.attach(app.config['UPLOAD_FOLDER'], filename)
+
+	
+		
+		
+		
+
+		# <sends but only from directory>
+		# with open(request.form['attachment'], 'rb') as f:
+			# msg.attach("somefile", 'image/jpg', f.read())
 
 		# if request.files:
 		# 	attachment = request.files['attachment']
@@ -50,7 +103,7 @@ def contactform():
 		mail.send(msg)
 		return "Successfully  sent message!"
 		
-	return render_template('contactform.html')
+	return render_template('/contactform.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
